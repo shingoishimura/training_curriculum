59dd59c9ebc395651fe1c1bddc85bf082f031115
shingoishimura <shingoishimuratw@gmail.com>
2021-01-18 15:55:00 +0900

    Revert "Merge pull request #8 from shingoishimura/second_training"
    This reverts commit afcc3551f644bd2b7cc2c31f705b0bbe6e7e3886, reversing
    changes made to 1dcd77a5ee77035c9d6d1ffae2fb4fe41a9239a6.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,24 +8,18 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    @plan = Plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
-
-  def getWeek 
-    
-    
-    
-
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-   
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -39,31 +33,10 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-        
-    
       end
-
-    
-     
-      wday_num = Date.today.wday
-      # wdayメソッドを用いて取得した数値
-
-      wday_num += x
-  
-    
-      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7
-        
-      end
-  
-  
-
-      
-      days = { month: (@todays_date + x).month, date:  @todays_date.day + x, plans: today_plans, wdays: wdays[wday_num]}
-      
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
-  
     end
+
   end
-  
 end
